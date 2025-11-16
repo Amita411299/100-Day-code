42d95eb42a7dc737f027e2c68b599db736a34cc7
Amita411299 <nishu3071singh@gmail.com>
2025-11-16 15:11:28 +0530

    Create D19-Q38.c

---
+++ D19-Q38.c
@@ -0,0 +1,24 @@
+Q38-Write a program to find the sum of digits of a number.
+Input 1:
+123
+Output 1:
+6
+Input 2:
+999
+Output 2:
+27
+  CODE-
+  #include <stdio.h>
+
+int main() {
+    int num, sum = 0;
+    scanf("%d", &num);
+
+    while (num > 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+
+    printf("Sum of digits: %d\n", sum);
+    return 0;
+}
