06d8606fcf8a08e60c08486e38c87ad707a6bd3b
Amita411299 <nishu3071singh@gmail.com>
2025-11-16 15:22:28 +0530

    Create D22-Q44.c

---
+++ D22-Q44.c
@@ -0,0 +1,28 @@
+Q44-Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
+Input 1:
+3
+Output 1:
+Approximate sum: 3.3
+Input 2:
+5
+Output 2:
+Approximate sum: 4.4
+  CODE-
+  #include <stdio.h>
+
+int main() {
+    int n;
+    double sum = 0.0;
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        double numerator = 2*i - 1;
+        double denominator = 2*i;
+        sum += numerator / denominator;
+    }
+
+    printf("Sum = %.4f\n", sum);
+
+    return 0;
+}
