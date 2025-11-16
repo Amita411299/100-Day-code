44fa5158e9476372328b38bcc14e395cfc9d3e94
Amita411299 <nishu3071singh@gmail.com>
2025-11-16 15:29:45 +0530

    Create D26-Q51.c

---
+++ D26-Q51.c
@@ -0,0 +1,34 @@
+Q51-Write a program to print the following pattern:
+    5
+   45
+  345
+ 2345
+12345
+Input 1:
+Output 1:
+    5
+   45
+  345
+ 2345
+12345
+  CODE-
+  #include <stdio.h>
+
+int main() {
+    for (int i = 5; i >= 1; i--) {
+
+        // print leading spaces
+        for (int s = 1; s < i; s++) {
+            printf(" ");
+        }
+
+        // print numbers from i to 5
+        for (int j = i; j <= 5; j++) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
